64f9c27642568f68250b7504dc130b7e856f09c9
rahulmamoria30 <rahulmamoria@gmail.com>
2024-10-15 18:26:44 +0530

    java backend

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        File file = new File("src/new_file.txt");
+//        file.createNewFile();
+//        System.out.println("New file created...");
+//        File file1 = new File("java_practice/java.txt");
+//        System.out.println(file1.createNewFile());
+
+        String os = System.getProperty(("os.name"));
+        System.out.println(os);
+
+    }
+}
